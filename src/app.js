c14386ef3a02eda43238a13daeab5e03ccb66dce
Josep M Viladegut <Josep-Maria.Viladegut@gft.com>
2016-09-28 20:55:35 +0200

    Comment and clear code

--- src/app.js
+++ src/app.js
@@ -1,84 +1,50 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var axios = require('axios');
 var request = require('sync-request');
 var REST_PORT = (process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
+/**
+ * Send Welcome
+ * Show an inline_keyboard actions in Telegram
+ * @param res
+ */
+function sendWelcome(res) {
+	console.log("WELCOME");
 
-app.post('/', function (req, res) {
-
-	if (req.body.result.action === "nearOffices") {
-		var response = request('POST', 'https://digitalbank-int.gftlabs.com/digitalbank-office/rest/office/branches/find', {
-			json: {
-				latitude: 41.491488,
-				longitude: 2.0732503,
-				radius: 10
-			}
-		});
-		var message = {
-			speech: "ApiBackendNodeViladegut Speech",
-			displayText: "This is my displayText from ViladegutApiBackend",
-			source: "BankBot",
-			data: response.getBody('utf8')
-		};
-		res.send(message);
-
-/*		axios.post('https://digitalbank-int.gftlabs.com/digitalbank-office/rest/office/branches/find', {
-			latitude: 41.491488,
-			longitude: 2.0732503,
-			radius: req.body.result.parameters.radius
+	//Create response for Telegram with INLINE_KEYBOARD
+	var telegramAnswer = {
+		text: "What do you want to do?",
+		reply_markup: JSON.stringify({
+			inline_keyboard: [
+				[{text: 'Show my contacts', url: "www.google.es"}],
+				[{text: 'Show the nearest offices', callback_data: "buttonTextCallback2"}]]
 		})
-			.then(function (response) {
-				//console.log(response.data.result);
-
-				var serviceResponse = createServiceResponse(response.data.result);
-
-				res.send(JSON.stringify(serviceResponse));
-			})
-			.catch(function (error) {
-				console.log(error);
-			});*/
-	}
-	else if (req.body.result.action === "showContacts") {
-		console.log("CONTACTS");
-		res.send({
-			speech: "ApiBackendNodeViladegut Speech",
-			displayText: "This is my displayText from ViladegutApiBackend",
-			source: "BankBot",
-			data: {telegram: {
-				reply_markup:{
-					keyboard: [
-						[{text: 'Yes, you are the bot of my life ❤', callback_data: "buttonTextCallback1"}],
-						[{text: 'No, sorry there is another one...', callback_data: "buttonTextCallback2"}]]
-				}
-			}}
-		});
-	}
-	//res.send('Hello World!');
-});
-
-app.listen(REST_PORT, function () {
-	console.log('Example app listening on port: ' + REST_PORT);
-});
-
-
-
-function createServiceResponse(data) {
-	console.log(data);
-	var message = {
-		speech: "ApiBackendNodeViladegut Speech",
-		displayText: "This is my displayText from ViladegutApiBackend",
-		source: "BankBot"
 	};
-	return message;
+
+	res.send({
+		speech: "BankBotCXLAB Speech",
+		displayText: "What do you want to do?",
+		source: "BankBot",
+		data: {
+			telegram: telegramAnswer,
+			apibot: {text: "papapapappapapapa"} //Response for own bot
+		}
+	});
 }
 
-function reqWithSync() {
-	var res = request('POST', 'https://digitalbank-int.gftlabs.com/digitalbank-office/rest/office/branches/find', {
+/**
+ * GetNearOffices
+ * Get the nearest offices from DigitalBank server
+ * @param res
+ */
+function getNearOffices(res) {
+	console.log("OFFICES");
+
+	var officesResponse = request('POST', 'https://digitalbank-int.gftlabs.com/digitalbank-office/rest/office/branches/find', {
 		json: {
 			latitude: 41.491488,
 			longitude: 2.0732503,
@@ -86,6 +52,97 @@ function reqWithSync() {
 		}
 	});
 
-	return JSON.parse(res.getBody('utf8'));
-	//console.log(response);
+	var botResponse = {
+		speech: "BankBotCXLAB Speech",
+		displayText: "The nearest offices are: ",
+		source: "BankBot",
+		data: officesResponse.getBody('utf8')   //retrieve the response of the server
+	};
+	res.send(botResponse);
 }
+
+/**
+ * GetContacts
+ * Get contacts of the user
+ * CREATE INLINE_KEYBOARD RESPONSE FOR TELEGRAM
+ * @param res
+ */
+function getContacts(res) {
+	console.log("CONTACTS");
+
+	//Create static "INLINE_KEYBOARD"
+	var telegramAnswer = {
+		text : "Select user",
+		reply_markup : JSON.stringify({
+			inline_keyboard : ["Tim", "Monica", "Lisa", "Jane", "William"]
+		})
+	};
+
+	res.send({
+		speech: "BankBotCXLAB Speech",
+		displayText: "You contacts are: ",
+		source: "BankBot",
+		data: {
+			telegram: telegramAnswer
+		}
+	});
+}
+
+/**
+ * Confirm Action
+ * Confirm action for a transfer/pay/send money....
+ * @param res
+ */
+function confirmAction(res) {
+	console.log("CONFIRMATION");
+
+	//Create KEYBOARD buttons for telegram
+	var telegramAnswer = {
+		text : "Operation summary Action...., Amount...., Addresses to...., Is it okey?",
+		reply_markup : JSON.stringify({
+			keyboard : [["Yes", "No"]]
+		})
+	};
+
+	res.send({
+		speech: "BankBotCXLAB Speech",
+		displayText: "This is my displayText from ViladegutApiBackend",
+		source: "BankBot",
+		data: {
+			telegram: telegramAnswer,
+			apibot: {text: "Yes, No"}
+		}
+	});
+}
+
+
+app.post('/', function (req, res) {
+	console.log(req.body.result.action);
+
+	switch(req.body.result.action) {
+		case "sayWelcome":
+			sendWelcome(res);
+			break;
+
+		case "nearOffices":
+			getNearOffices(res);
+			break;
+
+		case "showContacts":
+			getContacts(res);
+			break;
+
+		case "confirmAction":
+			confirmAction(res);
+			break;
+
+		default:
+			res.send();
+	}
+
+});
+
+
+app.listen(REST_PORT, function () {
+	console.log('Example app listening on port: ' + REST_PORT);
+});
